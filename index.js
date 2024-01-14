6820d27ec05b46e64efeeee3801c3fc89f3a8e4c
William Harrison <william@williamdavidharrison.com.au>
2024-01-14 18:39:50 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,133 @@
+const axios = require("axios");
+const { Octokit } = require("@octokit/rest");
+
+const githubToken = "GITHUB_PAT_TOKEN";
+const apiUrl = "https://raw-api.is-a.dev/";
+
+let amountScanned = 0;
+const domainsToSkip = ["@"];
+
+async function fetchData() {
+    try {
+        const response = await axios.get(apiUrl);
+        let data = response.data;
+
+        amountScanned = data.length;
+
+        const invalidDomains = [];
+        const invalidDomainData = [];
+        const invalidHosting = [];
+
+        for (const entry of data) {
+            const domain = entry.domain;
+            const domainUrl = `https://${domain}`;
+
+            // Skip if the domain is not pointing to a valid IP address
+            if (!entry.record.A && !entry.record.AAAA && !entry.record.CNAME && !entry.record.URL) continue;
+            // Skip if the domain is in the skip list
+            if (domainsToSkip.includes(entry.subdomain)) continue;
+
+            try {
+                await axios.head(domainUrl);
+            } catch (error) {
+                if (error.code === "ERR_TLS_CERT_ALTNAME_INVALID") continue;
+                if (entry.usesHosting) invalidHosting.push(domain);
+
+                console.error(`[ERROR] ${domain}: ${error.message}`);
+                invalidDomains.push(entry.subdomain);
+                invalidDomainData.push(entry);
+            }
+        }
+
+        if (invalidDomains.length > 0) {
+            await forkAndOpenPR(invalidDomains, invalidDomainData, invalidHosting);
+        } else {
+            console.log("No invalid domains found.");
+        }
+    } catch (error) {
+        console.error(`[ERROR] Fetching data: ${error.message}`);
+    }
+}
+
+async function forkAndOpenPR(invalidDomains, invalidDomainData, invalidHosting) {
+    const octokit = new Octokit({ auth: githubToken });
+
+    try {
+        // Fork the repository
+        const forkResponse = await octokit.repos.createFork({
+            owner: "is-a-dev",
+            repo: "register",
+        });
+
+        const forkedRepoFullName = forkResponse.data.full_name;
+
+        // Make changes in the forked repository
+        await deleteInvalidFiles(invalidDomains, forkedRepoFullName);
+
+        // Open a pull request
+        const prResponse = await octokit.pulls.create({
+            owner: "is-a-dev",
+            repo: "register",
+            title: "[no-rm] remove unresolvable domains",
+            body: `Scanned **${amountScanned}** domain${amountScanned === 1 ? "" : "s"} and found **${invalidDomains.length}** unresolvable domain${invalidDomains.length === 1 ? "" : "s"}.
+
+<details>
+<summary>Domain Owners</summary>
+
+${invalidDomainData.map((e) => `@${e.owner.username}: ${e.domain}(https://${e.domain})`).join("\n")}
+
+</details>
+`,
+            head: "is-a-dev-bot:main",
+            base: "main",
+        });
+
+        console.log(`Pull request opened: ${prResponse.data.html_url}`);
+    } catch (error) {
+        console.error(`[ERROR] Forking repository or opening PR: ${error.message}`);
+    }
+
+    // Save invalidHosting to txt file
+    if(invalidHosting.length > 0) {
+        const fs = require("fs");
+
+        const file = `invalidHosting-${Date.now()}.txt`;
+
+        fs.writeFile(file, invalidHosting.join("\n"), function (err) {
+            if (err) return console.log(err);
+            console.log(`Invalid domains using hosting have been saved to ${file}`);
+        });
+    }
+}
+
+async function deleteInvalidFiles(invalidDomains, repoFullName) {
+    const octokit = new Octokit({ auth: githubToken });
+
+    for (const domain of invalidDomains) {
+        const fileName = `domains/${domain}.json`;
+
+        try {
+            const fileContent = await octokit.repos.getContent({
+                owner: "is-a-dev",
+                repo: "register",
+                path: fileName,
+            });
+
+            const sha = fileContent.data.sha;
+
+            await octokit.repos.deleteFile({
+                owner: repoFullName.split("/")[0],
+                repo: repoFullName.split("/")[1],
+                path: fileName,
+                message: `chore: remove ${domain}.is-a.dev`,
+                sha: sha,
+            });
+
+            console.log(`Deleted ${domain}.is-a.dev`);
+        } catch (error) {
+            console.error(`[ERROR] Deleting ${domain}: ${error.message}`);
+        }
+    }
+}
+
+fetchData();
